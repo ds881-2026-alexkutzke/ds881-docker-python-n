2af3714da7cacb7710a9ba953e9147a9789d0966
Henrique <henriquekkrrr@gmail.com>
2026-03-23 20:24:30 -0300

    Adicionado nome para container

--- script-henrique.sh
+++ script-henrique.sh
@@ -2,4 +2,4 @@ echo "Subindo Container"
 
 docker build -f henrique.Dockerfile -t henrique-app:v1 .
 
-docker run --rm -d -p 8080:8080 henrique-app:v1
+docker run --rm -d -p 8080:8080 --name calculadora-henrique-docker henrique-app:v1
